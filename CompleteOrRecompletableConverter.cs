3067d7ead6590e0ba71ab37b5d446f230b871779
Dustin Hengel <djhyv2@mail.missouri.edu>
2019-09-05 10:49:28 -0500

    Removed Video File

---
+++ CompleteOrRecompletableConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RunescapeDailyTracker
+{
+    class CompletedOrRecompletableConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Task task = (Task)value;
+            return task.NotCompleted || task.Recompletable;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return true;
+        }
+    }
+}
